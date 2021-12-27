87d399d8f768f333f4f564e20b7a58cb6e5d8bfe
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-27 15:07:53 -0500

    commit tree 310dcb82c4a687938f658894138b0f6c2492e92f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter((driver) => driver.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => {
+    const test = driver.indexOf(string)
+    if (test === 0) return true
+    return false
+  })
+}
+
+function matchName (drivers, string) {
+  return drivers.filter(driverObj => driverObj.name.toLowerCase() === string.toLowerCase())
+}
